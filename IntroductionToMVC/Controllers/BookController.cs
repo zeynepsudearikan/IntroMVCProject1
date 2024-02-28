b11157bf0c6c8a4808bec7377359d19cfd8b5ab0
arika <arika@DESKTOP-FKPEQQP>
2024-02-28 14:33:40 +0300

    Proje dosyası ekle.

---
+++ IntroductionToMVC/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using IntroductionToMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace IntroductionToMVC.Controllers
+{
+    public class BookController : Controller
+    {
+
+
+        //book/display?name=Yüzüklerin Efendisi&author=Tolkien&pageNumber=1026&dueDate=2024-02-04
+        //yazıldığında istenen bilgiler çıkar
+        public IActionResult Display(string name, string author, int pageNumber, DateTime dueDate)
+        {
+            Book book = new Book
+            {
+                Name = name,
+                Author = author,
+                pageNumber = pageNumber,
+                dueDate = dueDate
+            };
+            return View(book);
+        }
+
+        
+    }
+}
---
+++ IntroductionToMVC/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace IntroductionToMVC.Models
+{
+    public class Book
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int pageNumber { get; set; }
+        public DateTime dueDate { get; set; }
+    }
+}
